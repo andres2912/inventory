c3c6ef62ed16af5f040565dcd03997b65e84acaa
Your Name <loperandresfl1@gmail.com>
2024-12-27 13:07:48 -0500

    Configuración inicial

---
+++ src/test/java/com/efitel/inventary/InventaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efitel.inventary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
