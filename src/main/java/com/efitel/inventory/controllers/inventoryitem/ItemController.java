6adca351306be3e972eb055e7e0ac89fce559b1b
andres2912 <142177247+andres2912@users.noreply.github.com>
2025-08-20 14:55:01 -0500

    Revert "Revert "Category and Item CRUD implementation""

---
+++ src/main/java/com/efitel/inventory/controllers/inventoryitem/ItemController.java
@@ -0,0 +1,59 @@
+package com.efitel.inventory.controllers.inventoryitem;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.efitel.inventory.models.dto.inventoryItem.ItemDTO;
+import com.efitel.inventory.models.entity.inventoryItem.ItemEntity;
+import com.efitel.inventory.services.inventoryItem.ItemService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/inventoryItem")
+public class ItemController {
+	@Autowired
+	ItemService itemService;
+
+	@PostMapping
+	ResponseEntity<ItemEntity> createUpdateItem(@Valid @RequestBody ItemDTO Item) {
+		return new ResponseEntity<>(itemService.createUpdateItem(Item), HttpStatus.CREATED);
+	}
+
+	@GetMapping("/getItemById/{itemId}")
+	ResponseEntity<ItemEntity> findItemById(@PathVariable Long itemId) {
+		return new ResponseEntity<>(itemService.findItemById(itemId), HttpStatus.OK);
+	}
+
+	@GetMapping("/getItemsByName/{itemName}")
+	ResponseEntity<List<ItemEntity>> getItemsByName(@PathVariable String itemName) {
+		return new ResponseEntity<>(itemService.findItemsByName(itemName), HttpStatus.OK);
+	}
+
+	@GetMapping("/getItemByName/{itemName}")
+	ResponseEntity<ItemEntity> getItembyName(@PathVariable String itemName) {
+		return new ResponseEntity<>(itemService.findItemByName(itemName), HttpStatus.OK);
+	}
+
+	@GetMapping("/getAllItems")
+	ResponseEntity<List<ItemEntity>> getAllItems() {
+		return new ResponseEntity<>(itemService.getItems(), HttpStatus.OK);
+	}
+
+	@DeleteMapping("/deleteByItemName/{itemName}")
+	ResponseEntity<String> deleteByItemName(@PathVariable String itemName){
+		return new ResponseEntity<>(itemService.deleteByItemName(itemName), HttpStatus.OK);
+	}
+	
+
+}
---
+++ src/main/java/com/efitel/inventory/services/category/CategoryService.java
@@ -0,0 +1,80 @@
+package com.efitel.inventory.services.category;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.efitel.inventory.models.dto.category.CategoryDTO;
+import com.efitel.inventory.models.dto.category.UpdateCategoryDTO;
+import com.efitel.inventory.models.entity.category.CategoryEntity;
+
+public interface CategoryService {
+	/**
+	 * Create and save a new category using the provided {@link CategoryEntity}.
+	 * 
+	 * @param categoryEntity The category entity to be created. Must not be
+	 *                       {@code null}.
+	 * @return The created {@link CategoryEntity} with generated ID field populated.
+	 */
+	CategoryEntity createCategory(CategoryDTO categoryEntity);
+
+	/**
+	 * Retrieves a category by its unique identifier.
+	 * 
+	 * @param categoryId The ID of the category to retrieve. Must not be
+	 *                   {@code null}.
+	 * @return {@link CategoryEntity} with the specified ID, or {@code null} if no
+	 *         category is found.
+	 */
+	CategoryEntity findCategoryById(Long categoryId);
+
+	/**
+	 * Retrieves {@link CategoryEntity} that exactly matches the given name, ignoring
+	 * case sensitivity.
+	 * 
+	 * @param categoryName The exact name of the category to search. Must not be
+	 *                     {@code null}.
+	 * @return The matching {@link CategoryEntity}, or {@code null} if no match is found.
+	 */
+	CategoryEntity getCategoryByName(String categoryName);
+
+	/**
+	 * Retrieves a list of categories whose names contain the given string. ignoring
+	 * case sensitivity.
+	 * 
+	 * @param categoryName The string to search for within category name. Must not
+	 *                     be {@code null}.
+	 * @return A list of {@link CategoryEntity} objects whose names contain the
+	 *         provided string. The list may be empty if no matching categories are
+	 *         found.
+	 */
+	List<CategoryEntity> getCategoriesByName(String categoryName);
+
+	/**
+	 * Retrieves all categories.
+	 * 
+	 * @return A list of {@link CategoryEntity} objects representing all categories.
+	 */
+
+	List<CategoryEntity> getCategories();
+
+	/**
+	 * Update a category by its name.
+	 * 
+	 * @param categoryDTO must contain the current category name and the new name.
+	 *                    Both must not be {@code null}. The description is optional
+	 *                    description.
+	 * @return The updated {@link CategoryEntity}.
+	 */
+
+	CategoryEntity updateCategoryByName(UpdateCategoryDTO categoryDTO);
+
+	/**
+	 * Delete a category by its name, ignoring case sensitivity.
+	 * 
+	 * @param categoryName The name of the category to delete. Must not be
+	 *                     {@code null}.
+	 * @return A confirmation message indicating the success of the operation.
+	 */
+	String deleteByCategoryName(String categoryName);
+
+}
