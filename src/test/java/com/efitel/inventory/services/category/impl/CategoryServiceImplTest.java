3c609f6bec980bac472ecbad9d0664a1037a3b4b
Your Name <loperandresfl1@gmail.com>
2025-09-15 20:38:12 -0500

    createCategoryTest

--- src/test/java/com/efitel/inventory/services/category/impl/CategoryServiceImplTest.java
+++ src/test/java/com/efitel/inventory/services/category/impl/CategoryServiceImplTest.java
@@ -20,57 +20,57 @@ import com.efitel.inventory.models.entity.category.CategoryEntity;
 import com.efitel.inventory.repository.category.CategoryRepository;
 import com.efitel.inventory.services.category.CategoryService;
 
-
 @ExtendWith(MockitoExtension.class)
 class CategoryServiceImplTest {
-	
+
 	@InjectMocks
 	private CategoryServiceImpl categoryServiceImpl;
-	
+
 	@Mock
 	private CategoryRepository categoryRepository;
-	
+
 	@Mock
 	CategoryMapper categoryMapper;
-	
-	
-	
+
 	@Test
 	void createCategoryTest() {
-		CategoryDTO categoryToSave  = new CategoryDTO();
-		categoryToSave .setCategoryName("categoryTest");
-		categoryToSave .setDescription("category description");
-		
+		CategoryDTO categoryToSave = new CategoryDTO();
+		categoryToSave.setCategoryName("categoryTest");
+		categoryToSave.setDescription("category description");
+
 		CategoryEntity mappedEntity = new CategoryEntity();
-        mappedEntity.setCategoryName("categoryTest");
-        mappedEntity.setDescription("category description");
-		
+		mappedEntity.setCategoryName("categoryTest");
+		mappedEntity.setDescription("category description");
+
 		CategoryEntity savedEntity = new CategoryEntity();
-        savedEntity.setCategoryId(1L);
-        savedEntity.setCategoryName("categoryTest");
-        savedEntity.setDescription("category description");
-        
-        CategoryDTO savedCategoryDto = new CategoryDTO();
-        savedCategoryDto.setCategoryId(1L);
-        savedCategoryDto.setCategoryName("categoryTest");
-        savedCategoryDto.setDescription("category description");
-        
-		when(categoryMapper.toCategoryEntity(categoryToSave )).thenReturn(mappedEntity);
+		savedEntity.setCategoryId(1L);
+		savedEntity.setCategoryName("categoryTest");
+		savedEntity.setDescription("category description");
+
+		CategoryDTO savedCategoryDto = new CategoryDTO();
+		savedCategoryDto.setCategoryId(1L);
+		savedCategoryDto.setCategoryName("categoryTest");
+		savedCategoryDto.setDescription("category description");
+
+		when(categoryMapper.toCategoryEntity(categoryToSave)).thenReturn(mappedEntity);
 		when(categoryMapper.toCategoryDTO(savedEntity)).thenReturn(savedCategoryDto);
-        when(categoryRepository.save(any(CategoryEntity.class))).thenReturn(savedEntity);
-		
-		
-		CategoryDTO savedCategory= categoryServiceImpl.createCategory(categoryToSave);
-		
+		when(categoryRepository.save(any(CategoryEntity.class))).thenReturn(savedEntity);
+
+		CategoryDTO savedCategory = categoryServiceImpl.createCategory(categoryToSave);
+
 		assertNotNull(savedCategory);
 		assertEquals(savedCategoryDto.getCategoryId(), savedCategory.getCategoryId());
 		assertEquals(savedCategoryDto.getCategoryName(), savedCategory.getCategoryName());
-		assertEquals(savedCategoryDto.getDescription(), savedCategory.getDescription());	
-		
+		assertEquals(savedCategoryDto.getDescription(), savedCategory.getDescription());
+
 		InOrder inOrder = Mockito.inOrder(categoryMapper, categoryRepository);
 		inOrder.verify(categoryMapper).toCategoryEntity(categoryToSave);
 		inOrder.verify(categoryRepository).save(any(CategoryEntity.class));
 		inOrder.verify(categoryMapper).toCategoryDTO(savedEntity);
 	}
-
+	
+	@Test
+	void findCategoryByIdTest() {
+		
+	}
 }
